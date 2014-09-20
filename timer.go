0bfcf8f83f7625e0c969baf9bada7308e770d1ed
Taru Karttunen <taruti@taruti.net>
2014-09-20 17:10:14 +0300

    Fix Elapsed

--- timer.go
+++ timer.go
@@ -16,7 +16,7 @@ func NewElapsed() *Elapsed {
 func (t Elapsed) Current() time.Duration {
 	n := int64(Now() - t.t0)
 	x := time.Now().UnixNano() - t.x0
-	if x-2000000000 < n || x+2000000000 > n {
+	if x-2000000000 > n || x+2000000000 < n {
 		return time.Duration(n)
 	}
 	return time.Duration(x)
